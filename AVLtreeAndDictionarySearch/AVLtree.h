4de1eed39f00eac73557a79a681f52235a1824d0
Kirill Makarov <kir-maka@yandex.ru>
2015-04-28 12:12:30 +0600

    Заготовка для рекурсивной вставки

--- AVLtreeAndDictionarySearch/AVLtree.h
+++ AVLtreeAndDictionarySearch/AVLtree.h
@@ -9,6 +9,13 @@ private:
 	AvlNode* parent;
 	AvlNode* left;
 	AvlNode* right;
+
+	//-1, 0, 1
+	char balance;
+
+public:
+	AvlNode(Key key_from_user):key(key_from_user), parent(0), left(0), right(0){}
+
 };
 
 //=============================================================================
@@ -19,15 +26,17 @@ template <class Key>
 class AVLtree {
 private:
 	AvlNode<Key>* root;
+	void recursive_insert (Key key, AvlNode<Key>* current_root);
+
 public:
 	AVLtree():root(0){}
 
 	/*
-		Добавляет элемент key в AVL дерево,
-		при необходимости происходит балансировка.
+	Добавляет элемент key в AVL дерево,
+	при необходимости происходит балансировка.
 
-		Если элемент уже добавлен в дерево, то повторного добавления не осуществляется,
-		то есть двух элементов с одинаковым ключом в AVL-дереве не существует.
+	Если элемент уже добавлен в дерево, то повторного добавления не осуществляется,
+	то есть двух элементов с одинаковым ключом в AVL-дереве не существует.
 	*/
 	void insert (Key key);
 };
--- AVLtreeAndDictionarySearch/AVLtree.hpp
+++ AVLtreeAndDictionarySearch/AVLtree.hpp
@@ -1,1 +1,21 @@
-﻿
+﻿#ifndef AVLTREE_HPP
+#define AVLTREE_HPP
+#include "AVLtree.h"
+
+template <class Key>
+void AVLtree<Key>::recursive_insert (Key key, AvlNode<Key>* current_root){
+}
+
+template <class Key>
+void AVLtree<Key>::insert(Key key){
+	if (AVLtree::root == nullptr)
+	{
+		root = new AvlNode<Key>(key);
+		return;
+	}
+
+	recursive_insert(key, root);
+}
+
+
+#endif
